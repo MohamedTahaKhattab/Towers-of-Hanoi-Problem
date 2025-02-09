3364a132552e23bc0ec0f9937d83df1fb56a9580
Mohamed Khattab <108592567+MohamedTahaKhattab@users.noreply.github.com>
2025-02-10 00:52:03 +0200

    Create tower_of_hanoi.h

---
+++ Solution/Using Recursion/tower_of_hanoi.h
@@ -0,0 +1,14 @@
+ #pragma once
+ classtower_of_hanoi
+ {
+ private:
+ intring;
+ charFristTower; 
+charSecondTower;
+ charThirdTower;
+ public:
+ voidhanoi(intring, char
+ FristTower, char
+ ThirdTower, char
+ SecondTower);
+ };
